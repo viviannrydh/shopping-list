8db86cbda6c60e159d0c1996fa455c6315f7790b
Viviann Rydh <rydh.vivian@gmail.com>
2020-11-05 09:49:37 +0100

    add lists

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ let addInput=document.getElementById('add-input');
 let add=document.getElementById('add');
 let toDoInput=document.getElementById('to-do-input');
 let toDoContainer=document.getElementById("to-do-container")
-let toDoInputArray=Array.from(addInput);
-let changeButtons=document.getElementsByClassName('change');
-let deleteButtons=document.querySelectorAll('.delete');
-let doneButtons=document.querySelectorAll('.done');
-let doneInput=document.querySelector('#done-input')
+let addInputArray=Array.from(addInput.value);
+
+
+
 
 
 
@@ -15,71 +14,81 @@ add.addEventListener('click',()=>{
     if(addInput.value.length==0){
         alert('OPS! The input form is empty! :(')
     } else {
-        toDoInput.value=addInput.value;
+        //toDoInput.value=addInput.value;
+        
+        createNewInputBox(addInput.value); 
         addInput.value='';
-        createNewInputBox();   
+          
     }
-})
+});
 
-
+/*
 // add event listener for all the 'Ändra' buttons
-changeButtons[0].addEventListener('click',()=>{
-if(toDoInput.value!=''){
-    toDoInput.style.borderColor='red';
-    if(changeButtons[0].innerHTML==='Ändra'){
-        changeButtons[0].innerHTML="Spara";
-    } else {
-        toDoInput.style.borderColor='lightgrey';
-        changeButtons[0].innerHTML="Ändra";
-    }
-} else {
-    alert ('Sorry, you can not change/save empty values');
-}
+let changeButtons=document.getElementsByClassName('change');
+let changeButtonsArray=Array.from(changeButtons);
+changeButtonsArray.forEach(changeButton=>{ 
+    changeButton.addEventListener('click',()=>{
+        if(toDoInput.value!=''){
+            toDoInput.style.borderColor='red';
+            if(button.innerHTML==='Ändra'){
+                button.innerHTML="Spara";
+            } else {
+                toDoInput.style.borderColor='lightgrey';
+                button.innerHTML="Ändra";
+            }
+        } else {
+            alert ('Sorry, you can not change/save empty values');
+        }
 }
 )
-// add event listener for all the 'Radera' buttons
+}
+);
 
-deleteButtons[0].addEventListener('click',()=>{
-        deleteButtons[0].parentNode.parentNode.remove();
-})
+// add event listener for all the 'Radera' buttons
+let deleteButtons=document.querySelectorAll('.delete');
+deleteButtons.forEach(deleteButton=>{ 
+    deleteButton.addEventListener('click',()=>{
+        deleteButton.parentNode.parentNode.remove();
+})});
 
 // add event listener for all the 'färdig' buttons
-doneButtons[0].addEventListener('click',()=>{
+let doneButtons=document.querySelectorAll('.done');
+let doneInput=document.querySelector('#done-input')
+doneButtons.addEventListener('click',()=>{
     doneInput.value=toDoInput.value;
     toDoInput.parentNode.remove();
 
-})
+});*/
 
 
 
 // this function is defined for creating new input groups, based on how many times the user add text.
 
-function createNewInputBox(){
+function createNewInputBox(userInput){
 
     let toDoBox=document.createElement('div');
     toDoBox.className='input-group';
     toDoContainer.appendChild(toDoBox);
 
-    let newToDoInput=document.createElement('input');
-    newToDoInput.type='text';
-    newToDoInput.className="form-control";
-    toDoBox.appendChild(newToDoInput);
+    let toDoInput=document.createElement('input');
+    toDoInput.type='text';
+    toDoInput.className="form-control";
+    toDoInput.value=userInput;
+    toDoBox.appendChild(toDoInput);
 
-    let changeButton=document.createElement("BUTTON")
-     changeButton.innerHTML = "Ändra";  
-     changeButton.className="btn btn-outline-secondary change";                 
-     toDoBox.appendChild(changeButton);
+    let changeButtons=document.createElement("BUTTON")
+     changeButtons.innerHTML = "Ändra";  
+     changeButtons.className="btn btn-outline-secondary change";                 
+     toDoBox.appendChild(changeButtons);
      
-     let doneButton=document.createElement("BUTTON")
-     doneButton.innerHTML = "Färdig"; 
-     doneButton.className="btn btn-outline-secondary change"  ;                
-     toDoBox.appendChild(doneButton);
+     let doneButtons=document.createElement("BUTTON")
+     doneButtons.innerHTML = "Färdig"; 
+     doneButtons.className="btn btn-outline-secondary done"  ;                
+     toDoBox.appendChild(doneButtons);
 
     
-     let deleteButton=document.createElement("BUTTON")
-     deleteButton.innerHTML = "Radera";  
-     deleteButton.className="btn btn-outline-secondary change";                 
-     toDoBox.appendChild(deleteButton);
-}
-
-
+     let deleteButtons=document.createElement("BUTTON")
+     deleteButtons.innerHTML = "Radera";  
+     deleteButtons.className="btn btn-outline-secondary delete";                 
+     toDoBox.appendChild(deleteButtons);
+};
